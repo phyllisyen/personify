c3c22e849f7b896e7218c94de7ec1fa25f42a35d
phyllisyen <phy.yenn13@gmail.com>
2023-07-04 00:31:51 +0800

    hide api key

--- duplicate.py
+++ duplicate.py
@@ -1,6 +1,6 @@
 import os
 import openai
-openai.api_key = os.getenv("BOT_KEY")
+openai.api_key = os.getenv("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")
 
 completion = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
